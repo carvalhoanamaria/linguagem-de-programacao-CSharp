f3b8d728572514f1a6960fa59ad0870aebe639ea
Ana Maria  Carvalho <33851180+carvalhoanamaria@users.noreply.github.com>
2020-06-20 17:45:13 -0300

    Adicionando projeto troca de lâmpada 1.0

---
+++ trocaDeLampada/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace trocaDeLampada
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char acenteu = 's';
+
+
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine("Ir para o "+  i + " soquete");
+                Console.WriteLine(" ");
+                do
+                {   
+                    if (acenteu == 'n')
+                    {
+                        Console.WriteLine("Retirar a lâmpada.");
+                        Console.WriteLine("Colocar a lâmpada nova.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Adicionar o interruptor.");
+                    }
+                    Console.Write("A lâmpada não acenteu? (s/n)");
+                    acenteu = char.Parse(Console.ReadLine());
+                } while (acenteu == 'n');
+
+                Console.WriteLine("Pegar uma escada.");
+                Console.WriteLine("Colocar a escada embaixo do soquete.");
+                Console.WriteLine("Buscar uma lâmpada.");
+                Console.WriteLine("Adicionar o interruptor.");
+                Console.WriteLine("Subir na escada.");
+                Console.WriteLine("Retirar a lâmapda queimada.");
+                Console.WriteLine("Colocar a lâmpada nova.");
+
+
+            }
+
+
+        }
+    }
+}
